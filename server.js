23f6ac62036086eacf850d9682fd2979d946f3a8
Wind-Chaser <ranaasr60@gmail.com>
2017-10-08 20:44:34 +0530

    url updated

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ app.use(express.static(__dirname));
 app.get('/',function (req,res){
   res.sendFile(__dirname+"/index.html");
 });
+app.get('/event',function (req,res){
+  res.sendFile(__dirname+"/events.html");
+});
+app.get('/contact',function (req,res){
+  res.sendFile(__dirname+"/contacts.html");
+});
 app.listen(process.env.PORT || 8080,function(){
     console.log("localhost at 8080",process.env.PORT);
 });
