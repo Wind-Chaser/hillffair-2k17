31a9b2e0c68e44d90c71dee27692276279abf81e
Wind-Chaser <ranaasr60@gmail.com>
2017-10-13 13:25:26 +0530

    vf

--- other/menu.js
+++ other/menu.js
@@ -12,12 +12,12 @@
 
 		clubItems[i].addEventListener("mouseleave", function(){
 				document.getElementsByClassName("eventsMenuSpiderman")[0].className = "eventsMenuSpiderman pos" + active;
-				document.getElementsByClassName("spiderWeb")[0].className = "spiderWeb pos" + active;	
+				document.getElementsByClassName("spiderWeb")[0].className = "spiderWeb pos" + active;
 		});
 
 		clubItems[i].addEventListener("click", function(){
 			toggleMenu();
-			
+
 				document.getElementsByClassName("events" + active)[0].className = "events" + active + " events outsideRight";
 			document.getElementsByClassName("events" + this.hoverId)[0].className = "events" + this.hoverId + " events insideBody";
 			active = this.hoverId;
@@ -29,14 +29,18 @@
 		if(menuState == 0){
 			menuState = 1;
 			menu.className = "left active";
+			club=document.getElementsByClassName("clubIcon")[0];
+			club.className="clubIcon clicked1";
 		}
 
 		else{
 			menu.className = "left unactive";
+			club=document.getElementsByClassName("clubIcon")[0];
+			club.className="clubIcon unclicked";
 			menuState = 0;
 		}
 	}
-	
+
 	document.getElementsByClassName("clubIcon")[0].addEventListener("click", toggleMenu);
 
 })();
